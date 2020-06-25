5d0a0c2514226cdfeebceb70bd314ff0fc4b546f
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-06-25 15:42:51 +0800

    refactor the images

--- src/Components/Hangman.js
+++ src/Components/Hangman.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import shortid from 'shortid'
-import img0 from "../Images/0.jpg";
-import img1 from "../Images/1.jpg";
-import img2 from "../Images/2.jpg";
-import img3 from "../Images/3.jpg";
-import img4 from "../Images/4.jpg";
-import img5 from "../Images/5.jpg";
-import img6 from "../Images/6.jpg";
+import hangmanImages from '../Helpers/hangmanImages';
 
 class Hangman extends Component {
-  /** by default, allow 6 guesses and use provided gallows images. */
   static defaultProps = {
     maxWrong: 6,
-    images: [img0, img1, img2, img3, img4, img5, img6]
+    images: hangmanImages()
   };
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.state = {
+      nWrong: 0,
+      guessed: new Set(),
+      answer: "apple"
+    };
     this.handleGuess = this.handleGuess.bind(this);
   }
 
@@ -50,13 +47,10 @@ class Hangman extends Component {
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
-      >
-        {ltr}
-      </button>
+      >{ltr}</button>
     ));
   }
 
-  /** render: render game */
   render() {
     return (
       <div className='Hangman'>
