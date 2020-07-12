818b2776298724c941edb8d32a2a3fbd6e1e02f7
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-12 18:36:09 +0800

    added home and instructions component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import Home from './Components/Home';
 import Hangman from "./Components/Hangman";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { start: false }
+  }
+
+  startGame = () => {
+    this.setState({ start: true })
+  }
+
   render() {
     return (
       <div className="App">
-        <Hangman />
+
+        {
+          this.state.start ? <Hangman /> : <Home start={this.startGame} />
+        }
+
       </div>
     );
   }
---
+++ src/Components/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    handleStartGame = () => {
+        this.props.start()
+    }
+
+    render() {
+        return (
+            <section className="Home-container">
+
+                <div className="Home-heading-container">
+                    <h1>Hangman</h1>
+                    <img src={require('../Images/6.jpg')} alt="Hangman" />
+                </div>
+
+                <div style={{ width: '500px' }}>
+
+                    <p>
+                        Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses. - Wikipedia
+                    </p>
+
+                    <p>
+                        This one gives you a word to guess, as well as hints. You are given 6 tries in heart form <span className='heart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="red" width="12" height="12" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
+                        </span>
+                        for each word.
+                    </p>
+
+                    <p>
+                        Each time you get a correct answer, You will get 1pt. else, you'll get only the correct answer. However, you can still proceed to the next word as long as you want.
+                    </p>
+
+                    <p>
+                        upon ending the game, please take time to send us your score and first name and let see if you are on top of everyone elses who played the game.
+                    </p>
+
+                    <button
+                        onClick={this.handleStartGame}
+                    >Start</button>
+
+                    <button
+                    >High Scores</button>
+
+                </div>
+
+            </section>
+        )
+    }
+}
+
+export default Home;
